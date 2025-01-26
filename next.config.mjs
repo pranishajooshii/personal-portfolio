46d18c6da2598bf9cfcf721c1d8261b1bd3b18a9
Pranisha Joshi <pranisha.joshi11@gmail.com>
2025-01-26 21:39:36 +0545

    changed favicon

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    turbopack: false, // Disable Turbopack
+  },
+};
 
 export default nextConfig;
--- src/components/navbar/LinkList.jsx
+++ src/components/navbar/LinkList.jsx
@@ -17,60 +17,65 @@ const LinkList = ({ isMobile }) => {
   };
 
   return (
-    <div className={`${isMobile ? "text-left" : "text-center"}`}>
-      <ul className={`flex ${isMobile ? "flex-col" : "flex-row"} gap-10`}>
-        <li
-          className={`hover:text-gray-500 ${
-            pathname === "/#projects" ? "underline" : ""
-          } ${isMobile ? "mt-10" : ""}`}
-        >
-          <button
-            onClick={() => scrollToSection("projects")}
-            className="text-sm md:text-lg xl:text-xl"
-          >
-            Projects
-          </button>
-        </li>
+    <>
+      
 
-        <li
-          className={`hover:text-gray-500 ${
-            pathname === "/#skills" ? "underline" : ""
-          }`}
-        >
-          <button
-            onClick={() => scrollToSection("skills")}
-            className="text-sm md:text-lg xl:text-xl"
-          >
-            Skills
-          </button>
-        </li>
+<div className={`${isMobile ? "text-left" : "text-center"}`}>
+  <ul className={`flex ${isMobile ? "flex-col" : "flex-row"} gap-10`}>
+    <li
+      className={`hover:text-gray-500 ${
+        pathname === "/#projects" ? "underline" : ""
+      } ${isMobile ? "mt-10" : ""}`}
+    >
+      <button
+        onClick={() => scrollToSection("projects")}
+        className="text-sm md:text-lg xl:text-xl"
+      >
+        Projects
+      </button>
+    </li>
 
-        <li
-          className={`hover:text-gray-500 ${
-            pathname === "/#experience" ? "underline" : ""
-          }`}
-        >
-          <button
-            onClick={() => scrollToSection("experience")}
-            className="text-sm md:text-lg xl:text-xl"
-          >
-            Experience
-          </button>
-        </li>
-        <li
-          className={`hover:text-gray-500 ${
-            pathname === "/#contact" ? "underline" : ""
-          }`}
-        >
-          <button
-            onClick={() => scrollToSection("contact")}
-            className="text-sm md:text-lg xl:text-xl"
-          >
-            Contact
-          </button>
-        </li>
-      </ul>
-    </div>
+    <li
+      className={`hover:text-gray-500 ${
+        pathname === "/#skills" ? "underline" : ""
+      }`}
+    >
+      <button
+        onClick={() => scrollToSection("skills")}
+        className="text-sm md:text-lg xl:text-xl"
+      >
+        Skills
+      </button>
+    </li>
+
+    <li
+      className={`hover:text-gray-500 ${
+        pathname === "/#experience" ? "underline" : ""
+      }`}
+    >
+      <button
+        onClick={() => scrollToSection("experience")}
+        className="text-sm md:text-lg xl:text-xl"
+      >
+        Experience
+      </button>
+    </li>
+    <li
+      className={`hover:text-gray-500 ${
+        pathname === "/#contact" ? "underline" : ""
+      }`}
+    >
+      <button
+        onClick={() => scrollToSection("contact")}
+        className="text-sm md:text-lg xl:text-xl"
+      >
+        Contact
+      </button>
+    </li>
+  </ul>
+</div>
+     
+    </>
   );
 };
 
